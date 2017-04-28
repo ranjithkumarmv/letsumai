044ff6ba17c2b744d86f2383a9a1aec4c9d0549d
Ranjith Kumar MV <ranjithecemit@gmail.com>
2017-04-29 03:39:05 +0530

    added validations

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,2 +1,6 @@
 class Customer < ApplicationRecord
+  validates :name, length: { maximum: 25 }, presence: true, format: { with: /\A[a-zA-Z]+\z/, message: "only allows letters" }
+  validates_format_of :email, :with => /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i, presence: true, length: { maximum: 25 }
+  validates :phonenumber, numericality: true , length: { minimum:10, maximum: 10 }, presence: true
+  validates :email, uniqueness: true
 end
